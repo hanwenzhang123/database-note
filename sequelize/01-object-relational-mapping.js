c9783047dc763caa2fe9ada79eb0e2f0e27c697c
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-01 16:54:37 -0400

    Create 01-object-relational-mapping.js

---
+++ sequelize/01-object-relational-mapping.js
@@ -0,0 +1,16 @@
+ORM (Object-Relational Mapping) tools that let you interact with a SQL-based database using JavaScript (instead of raw SQL)
+
+Sequelize is an ORM library for:
+
+SQLite
+MySQL
+MariaDB
+PostgreSQL
+Microsoft SQL Server
+
+
+
+$ npm install --save sequelize
+
+
+   
