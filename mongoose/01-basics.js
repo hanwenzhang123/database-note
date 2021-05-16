b9306d2458f826f07c9f2d01104b27df17550dd5
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-16 12:45:40 -0400

    Update 01-basics.js

--- mongoose/01-basics.js
+++ mongoose/01-basics.js
@@ -10,7 +10,7 @@ $npm install mongoose --save
 //Models like collections, are defined by passing a Schema instance to mongoose.model, schema is a blueprint
 //you need to make a model after declaring the schema
 
-//.then((data) => console.log(data));   //check the data
+//.then((data) => console.log(data));   //check the data in the shell
 
 //from the web
 const mongoose = require('mongoose');
