38ac6be143816eca36d74c40c18e9b67aa494c97
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-16 12:53:03 -0400

    Update 01-basics.js

--- mongoose/01-basics.js
+++ mongoose/01-basics.js
@@ -22,6 +22,18 @@ const kitty = new Cat({ name: 'Zildjian' });
 kitty.save().then(() => console.log('meow'));
 
 
+//then catch example
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/{name of the database}', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!!!")
+        console.log(err)
+    })
+
+
 //fruit example
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/fruitDB', {useNewUrlParser: true});
