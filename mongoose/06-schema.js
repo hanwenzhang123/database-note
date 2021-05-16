0dc02e7edf93d72907714b8992a696f138efc704
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-16 12:10:31 -0400

    Create 06-schema.js

---
+++ mongoose/06-schema.js
@@ -0,0 +1,76 @@
+https://mongoosejs.com/docs/schematypes.html
+
+String
+Number
+Date
+Buffer
+Boolean
+Mixed
+ObjectId
+Array
+Decimal128
+Map
+Schema
+
+
+const schema = new Schema({
+  name:    String,
+  binary:  Buffer,
+  living:  Boolean,
+  updated: { type: Date, default: Date.now },
+  age:     { type: Number, min: 18, max: 65 },
+  mixed:   Schema.Types.Mixed,
+  _someId: Schema.Types.ObjectId,
+  decimal: Schema.Types.Decimal128,
+  array: [],
+  ofString: [String],
+  ofNumber: [Number],
+  ofDates: [Date],
+  ofBuffer: [Buffer],
+  ofBoolean: [Boolean],
+  ofMixed: [Schema.Types.Mixed],
+  ofObjectId: [Schema.Types.ObjectId],
+  ofArrays: [[]],
+  ofArrayOfNumbers: [[Number]],
+  nested: {
+    stuff: { type: String, lowercase: true, trim: true }
+  },
+  map: Map,
+  mapOfString: {
+    type: Map,
+    of: String
+  }
+})
+
+// example use
+
+const Thing = mongoose.model('Thing', schema);
+
+const m = new Thing;
+m.name = 'Statue of Liberty';
+m.age = 125;
+m.updated = new Date;
+m.binary = Buffer.alloc(0);
+m.living = false;
+m.mixed = { any: { thing: 'i want' } };
+m.markModified('mixed');
+m._someId = new mongoose.Types.ObjectId;
+m.array.push(1);
+m.ofString.push("strings!");
+m.ofNumber.unshift(1,2,3,4);
+m.ofDates.addToSet(new Date);
+m.ofBuffer.pop();
+m.ofMixed = [1, [], 'three', { four: 5 }];
+m.nested.stuff = 'good';
+m.map = new Map([['key', 'value']]);
+m.save(callback);
+
+
+// 3 string SchemaTypes: 'name', 'nested.firstName', 'nested.lastName'
+const schema = new Schema({
+  name: { type: String },
+  nested: {
+    firstName: { type: String },
+    lastName: { type: String }
+  }
+});
