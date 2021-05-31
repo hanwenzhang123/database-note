be9d9f2114b02e12a61dea3102a826f7cddac55e
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-05-31 18:19:31 -0400

    Update 01-basics.js

--- sqlite/01-basics.js
+++ sqlite/01-basics.js
@@ -7,6 +7,7 @@ const sqlite3 = require('sqlite3')
 // open up the SQLite database in './db.sqlite'
 const db = new sqlite3.Database('./db.sqlite')
 
+
 //Retrieving All Rows
 db.all('SELECT * FROM TemperatureData ORDER BY year', (error, rows) => {
   if (error) {
@@ -15,4 +16,16 @@ db.all('SELECT * FROM TemperatureData ORDER BY year', (error, rows) => {
   printQueryResults(rows);
 });
 
+db.all("SELECT * FROM Dog", (error, rows) => {
+  printQueryResults(rows.find(row => row.id === 1));
+});
 
+
+//Retrieving A Single Row
+
+db.get("SELECT * FROM TemperatureData WHERE year = 1992")
+
+db.get("SELECT * FROM Dog WHERE owner_name = 'Charlie'", (error, row) => {
+  printQueryResults(row); 
+});
+  
