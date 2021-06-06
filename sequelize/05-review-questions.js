fecd5cf66532a04c27fac14c14dd11a83221ded8
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-05 22:41:27 -0400

    Update and rename review-questions to 05-review-questions.js

---
+++ sequelize/05-review-questions.js
@@ -0,0 +1,17 @@
+ORM stands for:
+Object Relational Mapping
+
+Which of these methods deletes an existing table each time you start your app, and recreates it from the model definition?
+sequelize.sync {{ forcee: true }}
+
+ To synchronize your models with the database, use the _____ method.
+ .sync();
+
+Which method tests the connection to the database?
+.authenticate()
+
+Which of the following best describes ORMs?
+ORM libraries give you the ability to interact with a database using your programming language of choice.
+
+A model in Sequelize represents a database _____.
+table
